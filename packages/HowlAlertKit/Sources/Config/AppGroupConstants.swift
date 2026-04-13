a69e07e15cac923e6b6ce58e8f6ccbe826689487
Nathanial Henniges <19924836+nathanialhenniges@users.noreply.github.com>
2026-04-12 20:08:56 -0500

    feat: HowlAlertKit Swift package with pace engine, constants, and tests

---
+++ packages/HowlAlertKit/Sources/Config/AppGroupConstants.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public enum AppGroupConstants: Sendable {
+    public static let appGroup = "group.com.mrdemonwolf.howlalert"
+    public static let cloudKitContainer = "iCloud.com.mrdemonwolf.howlalert"
+
+    public enum BundleIDs: Sendable {
+        public static let iOS = "com.mrdemonwolf.howlalert"
+        public static let watchOS = "com.mrdemonwolf.howlalert.watchkitapp"
+        public static let macOS = "com.mrdemonwolf.howlalert.mac"
+    }
+
+    public enum Brand: Sendable {
+        public static let navy = "#091533"
+        public static let cyan = "#0FACED"
+    }
+}
